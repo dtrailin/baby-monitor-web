02fed149099fa3cb672b338a46f251461f978ed5
Denis Trailin <denis.trailin@gmail.com>
2016-02-27 22:28:01 -0800

    cleaned up code

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 var io = require('socket.io')();
-
-var isMobile = false,
-    isClient = true;
-var playerMapping = {};
-var currentSockId;
-console.log("test");
 io.on('connect', function(socket) {
   console.log("connected");
   socket.on('cry', function(input) {
+    console.log("crying because..."+input);
     io.emit('cry', {speed: input});
-      console.log("crying because..."+input);
     });
 });
 io.listen(7777);
