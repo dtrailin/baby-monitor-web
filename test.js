f271670f7558939561a86eaf7fdceaa19d1a3abd
Denis Trailin <denis.trailin@gmail.com>
2016-02-27 23:32:33 -0800

    updated tester

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 console.log('1');
 // Connect to server
 var io = require('socket.io-client');
-var socket = io.connect('http://localhost:7777', {reconnect: true});
+var socket = io.connect('http://baby-monitor.azurewebsites.net:7777', {reconnect: true});
 
 console.log('2');
 
 // Add a connect listener
 socket.on('connect', function(sockets) {
   socket.emit('cry', "Wahhhh");
+});
+
+socket.on('cry', function(cry) {
+  socket.emit('cry', cry);
+  console.log(cry);
 });
 
 console.log('3');
