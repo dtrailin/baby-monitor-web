82bbdcf57e086e4777c3e7e2f13ef7f33d2b929a
Denis Trailin <denis.trailin@gmail.com>
2016-02-27 21:30:59 -0800

    fixed socket code

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var isMobile = false,
     isClient = true;
 var playerMapping = {};
 var currentSockId;
-
+io.on('connect', function(socket) {
   socket.on('cry', function(input) {
     io.emit('cry', {speed: input});
       console.log("crying because..."+input);
     });
-
+});
 io.listen(7777);
